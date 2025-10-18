1b036e84b2d1bb99c4c24d0c1c684d7885127d2d
nolanelson9506 <nolamaenelson@gmail.com>
2025-10-18 14:27:22 -0500

    Update Problem2.py

--- Problem2.py
+++ Problem2.py
@@ -1,4 +1,39 @@
 #Midterm Exam
 #Problem2.py
-#Name:
-#Date:
+#Name:Nola Nelson
+#Date:10/18/25
+
+
+def countAEIOU(text):
+    countA = 0
+    countE = 0
+    countI = 0
+    countO = 0
+    countU = 0
+
+    for i in range(len(text)):
+            letter = text[i]
+            if letter == 'A' or letter == 'a':
+                    countA = countA + 1
+            elif letter == 'E' or letter == 'e':
+                    countE = countE + 1
+            elif letter == 'I' or letter == 'i':
+                    countI = countI + 1
+            elif letter == 'O' or letter == 'o':
+                    countO = countO + 1
+            elif letter == 'U' or letter == 'u':
+                    countU = countU + 1
+    return countA, countE, countI, countO, countU
+
+def main():
+        sentence = input("Enter a word or sentence: ")
+        a, e, i, o, u = countAEIOU(sentence)
+        print("A: ", a)
+        print("E: ", e)
+        print("I: ", i)
+        print("O: ", o)
+        print("U: ", u)
+
+
+if __name__ == "__main__":
+        main()
